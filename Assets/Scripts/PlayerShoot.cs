e7f862e55062483f36a89727725ac910db465006
mhartz <mshartz5@gmail.com>
2017-10-14 17:38:25 -0400

    Shoot debug added

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+	public float shootDistance;
+	public float shootRadius;
+
+	void Update() {
+		if (Input.GetButton("Shoot"))
+			Shoot();
+	}
+	
+	public void Shoot() {
+		RaycastHit hit;
+
+		Debug.DrawRay(transform.position, transform.forward.normalized * shootDistance, Color.red, 0);
+		
+		if (
+			Physics.SphereCast(transform.position, shootRadius, transform.forward, out hit, shootDistance)
+			&& hit.collider.CompareTag("Enemy")
+		) {
+			Debug.Log("Hit EM!");
+		}
+	}
+}
